94fd5e422b79a3763678506bbbe32b6b304199f9
eyepatch17 <50337435+eyepatch17@users.noreply.github.com>
2019-11-26 21:24:17 +0530

    Add files via upload

---
+++ src/pages/homepage/homepage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './homepage.scss'
+import Directory from '../../components/Directory/directory'
+
+const Homepage = () =>(
+    <div>
+    <Directory />
+    </div>
+
+);
+
+export default Homepage;
